a685bcfadd8b71c731b15f0967f4d9bdeb183863
Chan Ryu <upriser@gmail.com>
2023-01-17 16:28:23 +1100

    wip

--- src/extension.ts
+++ src/extension.ts
@@ -1,69 +1,52 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
+import {
+    CompletionItem,
+    CompletionItemKind,
+    ExtensionContext,
+    Position,
+    TextDocument,
+    languages,
+} from 'vscode';
 
-import * as vscode from 'vscode';
+const knownAttributes = require('../known-attributes.json')
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
-    const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
-
-        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
-            // a simple completion item which inserts `Hello World!`
-            const simpleCompletion = new vscode.CompletionItem('Hello World!');
-
-            // a completion item that inserts its text as snippet,
-            // the `insertText`-property is a `SnippetString` which will be
-            // honored by the editor.
-            const snippetCompletion = new vscode.CompletionItem('Good part of the day');
-            snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
-            const docs: any = new vscode.MarkdownString("Inserts a snippet that lets you select [link](x.ts).");
-            snippetCompletion.documentation = docs;
-            docs.baseUri = vscode.Uri.parse('http://example.com/a/b/c/');
-
-            // a completion item that can be accepted by a commit character,
-            // the `commitCharacters`-property is set which means that the completion will
-            // be inserted and then the character will be typed.
-            const commitCharacterCompletion = new vscode.CompletionItem('console');
-            commitCharacterCompletion.commitCharacters = ['.'];
-            commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
-
-            // a completion item that retriggers IntelliSense when being accepted,
-            // the `command`-property is set which the editor will execute after 
-            // completion has been inserted. Also, the `insertText` is set so that 
-            // a space is inserted after `new`
-            const commandCompletion = new vscode.CompletionItem('new');
-            commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-            commandCompletion.insertText = 'new ';
-            commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
-
-            // return all completion items as array
-            return [
-                simpleCompletion,
-                snippetCompletion,
-                commitCharacterCompletion,
-                commandCompletion
-            ];
-        }
-    });
-
-    const provider2 = vscode.languages.registerCompletionItemProvider(
+    const sbssPropertyValueProvider = languages.registerCompletionItemProvider(
         'sbss',
         {
-            provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
-                const text = document.lineAt(position).text.substring(0, position.character - 1).trimEnd();
-                if (text.endsWith('display')) {
-                    return [
-                        new vscode.CompletionItem('block', vscode.CompletionItemKind.EnumMember),
-                        new vscode.CompletionItem('none', vscode.CompletionItemKind.EnumMember),
-                    ];
+            provideCompletionItems(document: TextDocument, position: Position) {
+
+                const isInPropertyList = () => {
+                    let logicalLineBeginPosition = position.with(undefined, 0);
+                    while (logicalLineBeginPosition.line > 0) {
+                        const previousLinePosition = position.with(logicalLineBeginPosition.line - 1, 0);
+                        if (document.lineAt(previousLinePosition).text.endsWith('\\'))
+                            logicalLineBeginPosition = previousLinePosition;
+                        else
+                            break;
+                    }
+
+                    const regex = new RegExp("^\\s*(@root|(#|%)[\\.\\w- ]+|/[/\\.\\w- ]+)\\s*:");
+                    return regex.test(document.lineAt(logicalLineBeginPosition).text);
+                };
+
+                if (!isInPropertyList()) {
+                    return undefined;
                 }
-                return undefined;
+
+                const text = document.lineAt(position).text.substring(0, position.character - 1);
+
+                Math.max(text.lastIndexOf(','), text.lastIndexOf(':'));
+                for (const key in knownAttributes) {
+                    if (text.endsWith(key)) {
+                        const values: [string] = knownAttributes[key]
+                        return values.map(value => new CompletionItem(value, CompletionItemKind.EnumMember));
+                    }
+                }
             }
         },
-        '=' // triggered whenever a '=' is being typed
+        '=' // triggered whenever '=' or ':' is being typed
     );
 
-    context.subscriptions.push(provider1, provider2);
+    context.subscriptions.push(sbssPropertyValueProvider);
 }
