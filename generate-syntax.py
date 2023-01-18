91bd3e482a7da183c7fdad385bd4d6b672421c7a
Chan Ryu <upriser@gmail.com>
2023-01-18 13:05:36 +1100

    Use known-attributes.json

--- generate-syntax.py
+++ generate-syntax.py
@@ -2,72 +2,16 @@
 
 import os
 import sys
-
-
-_KNOWN_ATTRIBUTES = {
-    'begin-new-page': 'yes|no',
-    'pack': 'yes|no',
-    
-    'clear': 'none|left|right|both',
-    'writing-mode': 'horizontal\\\\-tb|vertical\\\\-rl',
-    'display': 'none|block|list|list\\\\-item|table',
-    'shut-in-adaptives': 'yes|no',
-    'page-side': 'auto|verso|recto',
-    'font-weight': 'normal|bold',
-    'font-style': 'normal|italic',
-    'font-no-scale': 'yes|no',
-    'bullet-text-align': 'left|right|center',
-    'bullet-font-weight': 'normal|bold',
-    'bullet-font-style': 'normal|italic',
-    'bullet-font-no-scale': 'yes|no',
-    
-    'vertical-align': 'baseline|top|middle|bottom',
-    'line-break-mode': 'auto|word\\\\-wrap|character-wrap',
-    
-    'text-align': 'justify|center|right|left',
-    'text-justify': 'auto|distribute\\\\-all\\\\-lines',
-    'text-orientation': 'mixed|upright|sideways|sideways\\\\-right',
-    'text-combine-horizontal': 'none|all|digits [1-4]',
-    
-    'position': 'static|absolute|abs',
-    'adaptive': 'yes|no',
-    'flow': 'yes|no',
-    'flow-mode': 'content\\\\-wrap|object\\\\-wrap',
-    'align': 'top|bottom|left|right|center',
-    'avoid-bottom': 'yes|no',
-    
-    'border-collapse': 'none|collapse',
-    
-    'background-image-type': 'stretch|pattern',
-    
-    'page-header-hidden': 'yes|no',
-    'page-footer-hidden': 'yes|no',
-    'page-background-image-type': 'stretch|pattern',
-
-    'text-decoration':
-        '(underline|overline|line\\\\-through|cross\\\\-out|sidedot|side\\\\-dot|outline)' +
-        '(\\\\s+(underline|overline|line\\\\-through|cross\\\\-out|sidedot|side\\\\-dot|outline))*',
-    
-    'gravity': 'center|left|top|right|bottom|' +
-               'left\\\\-top|top\\\\-left|left\\\\-bottom|bottom\\\\-left|' +
-               'right\\\\-top|top\\\\-right|right\\\\-bottom|bottom\\\\-right',
-}
+import json
 
 _KNOWN_PAIR_PATTERN_TEMPLATE = r'''
-{
-    "match": "(__KEY__)\\s*__SEP__\\s*((__VALUE_REGEX__)[^__TERM__\\s]+)",
-    "captures": {
-        "1": { "name": "__STYLE_PROP_NAME__" },
-        "2": { "name": "__STYLE_INVALID__" }
-    }
-},
 {
     "match": "(__KEY__)\\s*__SEP__\\s*(((\\$[A-Z_]+)|(__VALUE_REGEX__)|('(__VALUE_REGEX__)')|(\"(__VALUE_REGEX__)\"))|([^__TERM__]*))",
     "captures": {
         "1": { "name": "__STYLE_PROP_NAME__" },
         "3": { "name": "__STYLE_PROP_VALUE__" },
         "4": { "name": "__STYLE_VARIABLE__" },
-        "__WRONG_VALUE_INDEX__": { "name": "__STYLE_INVALID__" }
+        "10": { "name": "__STYLE_INVALID__" }
     }
 }'''
 
@@ -143,14 +87,17 @@ def _read_file_content(filename):
 
 
 def _make_known_pair_str(sep, term):
+    with open('./known-attributes.json', 'r') as file:
+        known_attributes = json.load(file)
     prop_pair_patterns = []
-    for key, value_regex in _KNOWN_ATTRIBUTES.items():
+    for key, values in known_attributes.items():
+        # reverse sort to match "list-item" first and then "list"
+        values.sort(reverse=True)
         s = _KNOWN_PAIR_PATTERN_TEMPLATE\
                 .replace('__SEP__', sep)\
                 .replace('__TERM__', term)\
                 .replace('__KEY__', key.replace('-', '\\\\-'))\
-                .replace('__VALUE_REGEX__', value_regex)\
-                .replace('__WRONG_VALUE_INDEX__', str(10 + value_regex.count('(')))
+                .replace('__VALUE_REGEX__', '|'.join([value.replace('-', '\\\\-') for value in values]))
         prop_pair_patterns.append(s)
     s = _GENERAL_PAIR_PATTERN_TEMPLATE\
             .replace('__SEP__', sep)\
