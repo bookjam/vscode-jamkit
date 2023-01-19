e891866a543df0961bbbc78c11c189d2a272e46b
Chan Ryu <upriser@gmail.com>
2023-01-19 17:05:14 +1100

    Diagnostic - property value check in *.sbml (#15)

--- src/SbmlDiagnosticCollector.ts
+++ src/SbmlDiagnosticCollector.ts
@@ -1,31 +1,68 @@
 import { assert } from 'console';
 import * as vscode from 'vscode';
-import * as utils from './utils';
+import { getKnownAttributeValues } from './KnownAttributes';
 
 const BEGIN_PATTERN = /^\s*=begin(\s+([^:]+))?/;
 const END_PATTERN = /^\s*=end(\s+(.+))?/;
-const IF_PATTERN = /^\s*=if(\s+.+)?/;
+const OBJECT_PATTERN = /^\s*=(object|image)(\s+([^:]+))?/;
+const STYLE_PATTERN = /^\s*=style(\s+([^:]+))?/;
+const COMMENT_PATTERN = /^\s*=comment\b/;
+const IF_PATTERN = /^\s*=if\b/;
+const ELIF_PATTERN = /^\s*=elif\b/;
+const ELSE_PATTERN = /^\s*=else\b/;
 
 enum DirectiveType {
-    Begin,      // =begin
-    End,        // =end
-    Object,     // =object, =image
-    Comment,    // =comment
-    Style,      // =style
-    If,         // =if
-    Elif,       // =elif
-    Else,       // =else
-}
-
-interface TagInfo {
-    name: string;
-    index: number;
+    Begin,
+    End,
+    Object,
+    Style,
+    Comment,
+    If,
+    Elif,
+    Else,
 }
 
 interface DirectiveContext {
-    line: number;
     type: DirectiveType;
-    tag?: TagInfo;
+    line: number;
+    tag?: string;
+}
+
+enum PropertyTarget {
+    Unknown,
+    Section,
+    Object
+}
+
+function toPropertyTarget(directiveType: DirectiveType): PropertyTarget {
+    if (directiveType == DirectiveType.Begin)
+        return PropertyTarget.Section;
+    if (directiveType == DirectiveType.Object)
+        return PropertyTarget.Object;
+    assert(directiveType === DirectiveType.Style);
+    return PropertyTarget.Unknown;
+}
+
+enum PropertyListParseState {
+    BeforeName,
+    InName,
+    AfterName,
+    BeforeValue,
+    InValue,
+    AfterValue
+}
+
+interface PropertyListParseContext {
+    offset: number;
+    target: PropertyTarget;
+    state: PropertyListParseState;
+
+    nameBeginPos?: vscode.Position;
+    nameEndPos?: vscode.Position;
+    valueBeginPos?: vscode.Position;
+    valueEndPos?: vscode.Position;
+    valueQuoteChar?: string;
+    valueEscaped?: boolean;
 }
 
 export class SbmlDiagnosticCollector {
@@ -43,22 +80,48 @@ export class SbmlDiagnosticCollector {
         this.openContextStack.length = 0;
 
         let isConnectedLine = false;
+        let propListParseContext: PropertyListParseContext | null = null;
+
         for (let i = 0; i < this.document.lineCount; ++i) {
-            const lineText = this.document.lineAt(i).text.trimStart();
+            const lineText = this.document.lineAt(i).text;
 
             if (!isConnectedLine) {
                 const context = this.parseDirective(i, lineText);
 
                 if (context) {
                     this.handleContext(context);
+
+                    if (context.type == DirectiveType.Begin ||
+                        context.type == DirectiveType.Object ||
+                        context.type == DirectiveType.Style) {
+                        const propertyListMarkerIndex = lineText.indexOf(':');
+                        if (propertyListMarkerIndex > 0) {
+                            propListParseContext = {
+                                offset: propertyListMarkerIndex + 1,
+                                target: toPropertyTarget(context.type),
+                                state: PropertyListParseState.BeforeName
+                            };
+                        }
+                    }
                 } else {
-                    // text 
+                    // TODO: do text related stuff
                 }
+            }
+
+            if (propListParseContext) {
+                if (isConnectedLine) {
+                    propListParseContext.offset = 0;
+                }
+                this.parsePropertyList(i, lineText, propListParseContext);
             }
 
             // collect more diagnostics based on lineKind ...
 
             isConnectedLine = lineText.endsWith('\\');
+
+            if (!isConnectedLine) {
+                propListParseContext = null;
+            }
         }
 
         return this.diagnostics;
@@ -66,27 +129,31 @@ export class SbmlDiagnosticCollector {
 
     private parseDirective(line: number, lineText: string): DirectiveContext | undefined {
 
-        const getTagIndex = (tag: string, directiveOffset: number): number => {
-            const leadingSpeacesOffset = lineText.length - lineText.trimStart().length;
-            const index = lineText.indexOf(tag, leadingSpeacesOffset + directiveOffset + 1);
-            assert(index !== undefined);
-            return index;
-        };
-
         let m;
 
         if (m = lineText.match(BEGIN_PATTERN)) {
-            const tag = m[2] ? { name: m[2], index: getTagIndex(m[2], /*"=begin"*/ 6) } : undefined;
-            return { line, type: DirectiveType.Begin, tag };
+            return { type: DirectiveType.Begin, line, tag: m[2] };
         }
-
         if (m = lineText.match(END_PATTERN)) {
-            const tag = m[2] ? { name: m[2], index: getTagIndex(m[2], /*"=end"*/ 4) } : undefined;
-            return { line, type: DirectiveType.End, tag };
+            return { type: DirectiveType.End, line, tag: m[2] };
         }
-
+        if (m = lineText.match(OBJECT_PATTERN)) {
+            return { type: DirectiveType.Object, line, tag: m[2] };
+        }
+        if (m = lineText.match(STYLE_PATTERN)) {
+            return { type: DirectiveType.Style, line, tag: m[2] };
+        }
+        if (m = lineText.match(COMMENT_PATTERN)) {
+            return { type: DirectiveType.Comment, line };
+        }
         if (m = lineText.match(IF_PATTERN)) {
-            return { line, type: DirectiveType.If };
+            return { type: DirectiveType.If, line };
+        }
+        if (m = lineText.match(ELIF_PATTERN)) {
+            return { type: DirectiveType.Elif, line };
+        }
+        if (m = lineText.match(ELSE_PATTERN)) {
+            return { type: DirectiveType.Else, line };
         }
 
         return undefined;
@@ -103,12 +170,18 @@ export class SbmlDiagnosticCollector {
             if (openContext) {
                 assert(openContext.type == DirectiveType.Begin || openContext.type == DirectiveType.If);
 
-                if (context.tag && openContext.tag?.name !== context.tag.name) {
-                    const endTagRange = new vscode.Range(
-                        context.line, context.tag.index,
-                        context.line, context.tag.index + context.tag.name.length
-                    );
+                if (context.tag && openContext.tag !== context.tag) {
 
+                    const endTagRange = ((): vscode.Range => {
+                        const lineText = this.document.lineAt(context.line).text;
+                        const leadingOffset = lineText.length - lineText.trimStart().length;
+                        const endTagIndex = lineText.indexOf(context.tag, leadingOffset + 5);
+                        assert(endTagIndex !== undefined);
+                        return new vscode.Range(
+                            context.line, endTagIndex,
+                            context.line, endTagIndex + context.tag.length
+                        );
+                    })();
                     const relatedInfo = new vscode.DiagnosticRelatedInformation(
                         new vscode.Location(
                             this.document.uri,
@@ -119,7 +192,7 @@ export class SbmlDiagnosticCollector {
                     );
                     this.diagnostics.push({
                         message: openContext.type == DirectiveType.Begin ?
-                            `section tag mismatch: "${openContext.tag?.name ? openContext.tag?.name : ""}" != "${context.tag.name}"` :
+                            `section tag mismatch: "${openContext.tag ? openContext.tag : ""}" != "${context.tag}"` :
                             "if statment can't have an ending tag",
                         range: endTagRange,
                         severity: vscode.DiagnosticSeverity.Warning,
@@ -139,4 +212,132 @@ export class SbmlDiagnosticCollector {
             // TODO: ...
         }
     }
+
+    private parsePropertyList(line: number, lineText: string, context: PropertyListParseContext): void {
+
+        for (let i = context.offset; i < lineText.length; ++i) {
+            const ch = lineText[i];
+
+            if (ch == '\\' && i == lineText.length - 1) {
+                // line concatnation char - ignore.
+                break;
+            }
+
+            switch (context.state) {
+                case PropertyListParseState.BeforeName:
+                    if (ch !== ' ' && ch !== '\t') {
+                        context.nameBeginPos = new vscode.Position(line, i);
+                        context.state = PropertyListParseState.InName;
+                    }
+                    break;
+
+                case PropertyListParseState.InName:
+                    assert(context.nameBeginPos);
+                    if (ch == ' ' || ch == '=') {
+                        context.nameEndPos = new vscode.Position(line, i);
+                        context.state = (ch == ' ') ?
+                            PropertyListParseState.AfterName :
+                            PropertyListParseState.BeforeValue;
+                    }
+                    break;
+
+                case PropertyListParseState.AfterName:
+                    assert(context.nameBeginPos);
+                    assert(context.nameEndPos);
+                    if (ch == '=') {
+                        context.state = PropertyListParseState.BeforeValue;
+                    } else if (ch !== ' ' && ch !== '\t') {
+                        // unexpected residue... report?
+                    }
+                    break;
+
+                case PropertyListParseState.BeforeValue:
+                    assert(context.nameBeginPos);
+                    assert(context.nameEndPos);
+                    if (ch !== ' ' && ch !== '\t') {
+                        if (ch === "'" || ch === '"') {
+                            context.valueQuoteChar = ch;
+                            context.valueEscaped = false;
+                        }
+                        context.valueBeginPos = new vscode.Position(line, i);
+                        context.state = PropertyListParseState.InValue;
+                    }
+                    break;
+
+                case PropertyListParseState.InValue:
+                    assert(context.nameBeginPos);
+                    assert(context.nameEndPos);
+                    assert(context.valueBeginPos);
+                    if (context.valueQuoteChar) {
+                        if (ch === '\\') {
+                            context.valueEscaped = !context.valueEscaped;
+                        } else if (ch === context.valueQuoteChar) {
+                            if (!context.valueEscaped) {
+                                context.valueEndPos = new vscode.Position(line, i + 1);
+                                context.state = PropertyListParseState.AfterValue;
+                            }
+                        } else {
+                            context.valueEscaped = false;
+                        }
+                    } else {
+                        if (ch === ',' || ch === ' ' || ch === '\t') {
+                            context.valueEndPos = new vscode.Position(line, i);
+                            context.state = ch === ',' ?
+                                PropertyListParseState.BeforeName :
+                                PropertyListParseState.AfterValue;
+                        }
+                    }
+
+                    if (!context.valueEndPos && i == lineText.length - 1) {
+                        context.valueEndPos = new vscode.Position(line, i + 1);
+                    }
+                    break;
+
+                case PropertyListParseState.AfterValue:
+                    if (ch === ',') {
+                        context.state = PropertyListParseState.BeforeName;
+                    } else if (ch !== ' ' && ch !== '\t') {
+                        // unexepcted residue... report?
+                    }
+                    break;
+            }
+
+            if (context.valueEndPos) {
+                this.checkPropertyDiagnostic(
+                    new vscode.Range(context.nameBeginPos!, context.nameEndPos!),
+                    new vscode.Range(context.valueBeginPos!, context.valueEndPos)
+                );
+
+                context.nameBeginPos = undefined;
+                context.nameEndPos = undefined;
+                context.valueBeginPos = undefined;
+                context.valueEndPos = undefined;
+                context.valueQuoteChar = undefined;
+            }
+        }
+    }
+
+    // This doesn't handle escaped characters properly but that's ok here.
+    private quoteStripped(value: string): string {
+        if (value.length >= 2 && value[0] == value[value.length - 1] && (value[0] == '"' || value[0] == "'")) {
+            return value.substring(1, value.length - 1);
+        }
+        return value;
+    }
+
+    private checkPropertyDiagnostic(nameRange: vscode.Range, valueRange: vscode.Range): void {
+        const name = this.document.getText(nameRange);
+        const value = this.quoteStripped(this.document.getText(valueRange));
+
+        const knownValues = getKnownAttributeValues(name);
+
+        if (knownValues && !knownValues.includes(value)) {
+            this.diagnostics.push({
+                message: `"${value}" is not a valid value for "${name}"`,
+                range: valueRange,
+                severity: vscode.DiagnosticSeverity.Error
+            });
+        }
+    }
 }
+
