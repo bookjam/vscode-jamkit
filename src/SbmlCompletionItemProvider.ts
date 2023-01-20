474197bf95045b287ee8ea7d16e8837addf12419
Chan Ryu <upriser@gmail.com>
2023-01-21 09:20:11 +1100

    wip

--- src/SbmlCompletionItemProvider.ts
+++ src/SbmlCompletionItemProvider.ts
@@ -1,10 +1,31 @@
 import * as vscode from 'vscode';
 import * as utils from './utils';
 import { getKnownAttributeValues } from './KnownAttributes';
+import { SBML_PROP_LIST_PREFIX } from './patterns';
+import { PropertyListParser } from './PropertyParser';
 
-interface CompletionContext {
-    attributeName: string;
+type PropListContext = {
+    directive: string;
+    beginPos: vscode.Position;
+};
+
+enum PropertyTarget {
+    Unknown,
+    Section,
+    BlockObject,
+    InlineObject,
 }
+
+type PropertyNameCompletionContext = {
+    target: PropertyTarget;
+};
+
+type PropertyValueCompletionContext = {
+    target: PropertyTarget;
+    propName: string;
+};
+
+type CompletionContext = PropertyNameCompletionContext | PropertyValueCompletionContext;
 
 class CompletionContextParser {
     readonly document: vscode.TextDocument;
@@ -15,28 +36,36 @@ class CompletionContextParser {
         this.position = position;
     }
 
-    parseContext(): CompletionContext | undefined {
+    parse(): CompletionContext | undefined {
         const lineText = utils.getLineTextAt(this.document, this.position).substring(0, this.position.character);
         const triggerChar = lineText.trimEnd().slice(-1);
 
-        if (triggerChar === '=') {
-            if (!this.isInPropertList())
-                return undefined;
+        const propListContext = this.getPropListContext();
+        if (propListContext) {
+            const target = (() => {
+                if (propListContext.directive == "begin")
+                    return PropertyTarget.Section;
+                if (propListContext.directive == "object" || propListContext.directive == "image")
+                    return PropertyTarget.BlockObject;
+                return PropertyTarget.Unknown;
+            })();
+
+            //const propParser = new PropertyListParser();
 
             const text = lineText.substring(0, lineText.length - 1);
-            const attributeName = text.substring(Math.max(text.lastIndexOf(','), text.lastIndexOf(':')) + 1).trim();
-            return { attributeName };
+            const propName = text.substring(Math.max(text.lastIndexOf(','), text.lastIndexOf(':')) + 1).trim();
+            return { target, propName };
         }
-
-        return undefined;
     }
 
-    private sectionPropListPrefix = /^\s*=begin\s+([\.\w-]*)?\s*:/;
-    private objectPropListPrefix = /^\s*=(object|image)\s+([a-z-]+)\s*:/;
-
-    private isInPropertList(): boolean {
-        const lineText = utils.getLineTextAt(this.document, utils.getLogicalLineBeginPosition(this.document, this.position));
-        return this.sectionPropListPrefix.test(lineText) || this.objectPropListPrefix.test(lineText);
+    private getPropListContext(): PropListContext | null {
+        const pos = utils.getLogicalLineBeginPosition(this.document, this.position);
+        const text = utils.getLineTextAt(this.document, pos);
+        const m = text.match(SBML_PROP_LIST_PREFIX);
+        if (m && m[4] == ':') {
+            return { directive: m[1], beginPos: new vscode.Position(pos.line, text.indexOf(':')) };
+        }
+        return null;
     }
 }
 
@@ -50,7 +79,7 @@ export class SbmlCompletionItemProvider implements vscode.CompletionItemProvider
 
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
         const contextParser = new CompletionContextParser(document, position);
-        const context = contextParser.parseContext();
+        const context = contextParser.parse();
         if (context) {
             const values = getKnownAttributeValues(context.attributeName);
             if (values) {
