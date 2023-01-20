c4520ebf277dc2a0e69c8fd1102837e2b95bcd73
Chan Ryu <upriser@gmail.com>
2023-01-20 11:25:13 +1100

    Remove invalid char

--- src/SyntaxAnalyser.ts
+++ src/SyntaxAnalyser.ts
@@ -25,7 +25,7 @@ export class SyntaxAnalyser {
                 instance.updateDiagnostics(event.document);
             } else {
                 instance.clearDiagnostics(event.document);
-            } \
+            }
         }));
     }
 
