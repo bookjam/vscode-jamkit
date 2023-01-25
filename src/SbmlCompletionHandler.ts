efb0224e57d5f6787a33352f7de175df1efd190f
Chan Ryu <upriser@gmail.com>
2023-01-25 23:16:21 +1100

    wip

--- src/SbmlCompletionHandler.ts
+++ src/SbmlCompletionHandler.ts
@@ -2,6 +2,81 @@ import * as vscode from 'vscode';
 import { PropCompletionItemProvider } from './PropCompletionItemProvider';
 import { SbmlContextParser } from './SbmlContextParser';
 
+
+// TODO: read this from object-*.json
+const OBJECT_TYPES = ["tabbar",
+    "section",
+    "sbml",
+    "sbmls",
+    "image",
+    "label",
+    "button",
+    "textfield",
+    "text",
+    "checkbox",
+    "choices",
+    "progress",
+    "slider",
+    "timer",
+    "input",
+    "point",
+    "drawing",
+    "photo",
+    "photozoom",
+    "photoplus",
+    "photoroll",
+    "photoscroll",
+    "multiphoto",
+    "map",
+    "minimap",
+    "audio",
+    "video",
+    "webvideo",
+    "youtube",
+    "twitch",
+    "vimeo",
+    "webtoon",
+    "web",
+    "comic",
+    "pdf",
+    "sprite",
+    "effect",
+    "animation",
+    "chart",
+    "camera",
+    "blank",
+    "editor",
+    "image.qrcode",
+    "record",
+    "epub",
+    "ad",
+    "node",
+    "hub",
+    "spoke"];
+
+function shouldSuggestDirectives(document: vscode.TextDocument, position: vscode.Position, context: vscode.CompletionContext): boolean {
+    return (
+        context.triggerCharacter == '=' &&
+        document.lineAt(position.line).text.trim() == '=' &&
+        (
+            position.line == 0 ||
+            !document.lineAt(position.line - 1).text.endsWith('\\')
+        )
+    );
+}
+
+function getDirectiveCompletionItems() {
+
+    return ['begin', 'end', 'object', 'comment', 'style', 'if', 'else', 'elif'].map(directive => {
+        const item = new vscode.CompletionItem(directive, vscode.CompletionItemKind.Keyword);
+        if (directive == 'object') {
+            item.insertText = new vscode.SnippetString('object ${1|' + OBJECT_TYPES.join(',') + '|}: ');
+        }
+        return item;
+    }
+    );
+}
+
 export class SbmlCompletionHandler {
     static register(context: vscode.ExtensionContext) {
         context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
@@ -13,12 +88,8 @@ export class SbmlCompletionHandler {
                     _token: vscode.CancellationToken,
                     context: vscode.CompletionContext
                 ) {
-                    if (context.triggerCharacter == '=' &&
-                        document.lineAt(position.line).text.trim() == '=' &&
-                        (position.line == 0 || !document.lineAt(position.line - 1).text.endsWith('\\'))) {
-                        return ['begin', 'end', 'object', 'comment', 'style', 'if', 'else', 'elif'].map(directive =>
-                            new vscode.CompletionItem(directive, vscode.CompletionItemKind.Keyword)
-                        );
+                    if (shouldSuggestDirectives(document, position, context)) {
+                        return getDirectiveCompletionItems();
                     }
 
                     const contextParser = new SbmlContextParser(document, position);
