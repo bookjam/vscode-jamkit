b70c9c30b73c19a0a36c2d3be367e73985f5844f
Chan Ryu <upriser@gmail.com>
2023-01-25 22:37:54 +1100

    remove unnecessary code

--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,7 @@ import { SbmlCompletionHandler } from './SbmlCompletionHandler';
 import { SyntaxAnalyser } from './SyntaxAnalyser';
 import { PropConfigStore } from './PropConfigStore';
 
-let extensionPath: string | undefined;
-
-export function getExtensionPath(): string {
-    return extensionPath!;
-}
-
 export function activate(context: ExtensionContext) {
-    extensionPath = context.extensionPath;
-
     PropConfigStore.init(context);
     SyntaxAnalyser.register(context);
     SbssCompletionHandler.register(context);
