1ca341719cca6bf41b581cd262c61a9c0834452a
Chan Ryu <upriser@gmail.com>
2023-02-03 22:12:32 +1100

    wip

--- src/test/suite/utils.test.ts
+++ src/test/suite/utils.test.ts
@@ -7,4 +7,8 @@ suite('Utils Test Suite', () => {
         assert.strictEqual('#ffffff', utils.toString(new vscode.Color(1, 1, 1, 1)));
         assert.strictEqual('#ffffff00', utils.toString(new vscode.Color(1, 1, 1, 0)));
     });
+    test('unquote', () => {
+        assert.strictEqual('abc', utils.unquote('"abc"'));
+        assert.strictEqual('a"bc', utils.unquote('"a\\"bc"'));
+    });
 });
