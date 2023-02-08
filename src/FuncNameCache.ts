e0db832561747796098fe3b83c73426d57ef97df
Chan Ryu <upriser@gmail.com>
2023-02-08 22:34:16 +1100

    wip

--- src/FuncNameCache.ts
+++ src/FuncNameCache.ts
@@ -1,6 +1,7 @@
 import { assert } from 'console';
 import { readFileSync } from 'fs';
 import * as vscode from 'vscode';
+import * as acorn from 'acorn';
 
 /**
  * Keeps top-level function names in memory for *.js files.
@@ -38,23 +39,44 @@ export class FuncNameCache {
 
     private static parseFuncNames(filePath: string): string[] {
 
-        const TOP_LEVEL_FUNC_DEF = /^function\s+(\w[\w\d]+)\s*\(+/;
-
         const funcNames: string[] = [];
 
         try {
             const content = readFileSync(filePath, 'utf-8');
-            content.split(/\r?\n/).forEach(text => {
-                const m = text.match(TOP_LEVEL_FUNC_DEF);
-                if (m) {
-                    funcNames.push(m[1]);
-                }
-            });
+            const program = acorn.parse(content, { ecmaVersion: 'latest' });
+            if ('body' in program) {
+                (program.body as acorn.Node[]).forEach(node => {
+                    console.log(node);
+                    if (node.type === 'FunctionDeclaration') {
+                        const funcName = getNodeIdName(node);
+                        if (funcName)
+                            funcNames.push(funcName);
+                    }
+                    else if (node.type === 'VariableDeclaration' && 'declarations' in node) {
+                        (node.declarations as acorn.Node[]).forEach(node => {
+                            if (node.type !== 'VariableDeclarator')
+                                return;
+                            if ('init' in node && node.init instanceof acorn.Node && node.init.type === 'ArrowFunctionExpression') {
+                                const funcName = getNodeIdName(node);
+                                if (funcName)
+                                    funcNames.push(funcName);
+                            }
+                        });
+                    }
+                });
+            }
         }
         catch (e) {
             console.error(e);
         }
 
         return funcNames;
+    }
+}
+
+function getNodeIdName(node: acorn.Node): string | undefined {
+    if ('id' in node && node.id instanceof acorn.Node &&
+        'name' in node.id && typeof node.id.name === 'string') {
+        return node.id.name;
     }
 }
